076d9c63746d20235f294b2a6f4fcf7ee75310bb
James Madden <jamesbmadden@gmail.com>
2022-11-19 14:16:28 -0500

    Add media player popup

--- src/Components/MediaPlayer.jsx
+++ src/Components/MediaPlayer.jsx
@@ -4,7 +4,12 @@ import './MediaPlayer.css';
 export default function MediaPlayer () {
 
   return (
-    <div className="mediaPlayer"></div>
+    <div className="mediaplayer">
+      {/* Header is the portion that can be seen at all times. */}
+      <header>
+        <p>Img and podcast info here</p>
+      </header>
+    </div>
   );
 
 }
