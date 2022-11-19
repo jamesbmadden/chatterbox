a499603e53c968cde79085409e2e20eecbfd080b
James Madden <jamesbmadden@gmail.com>
2022-11-19 12:15:31 -0500

    Show page depending on auth status

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-<<<<<<< Updated upstream
 import Spotify from './Logic/spotify';
-=======
 import Signin from './Pages/Signin.js';
 import Home from './Pages/Home.js';
 
->>>>>>> Stashed changes
 import SpeechRecog from './SpeechRecognition';
 
 const spotify = new Spotify();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SpeechRecog></SpeechRecog>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (spotify.loggedIn) return <Home></Home>;
+  else return <Signin></Signin>;
 }
 
 export default App;
