a2781e4af930d97f11e774210470fbbe6d03c81f
James Madden <jamesbmadden@gmail.com>
2022-11-19 16:33:50 -0500

    Add mini player

--- src/Components/MediaPlayer.jsx
+++ src/Components/MediaPlayer.jsx
@@ -8,10 +8,15 @@ export default function MediaPlayer ({ spotify }) {
       <img className="mediaplayer-bg" src={spotify.nowPlaying.image}></img>
       {/* Header is the portion that can be seen at all times. */}
       <header>
-        <p>Img and podcast info here</p>
-        <button onClick={() => spotify.spotifyPlayer.togglePlay()}>Toggle Playback</button>
-        <button onClick={() => spotify.play('spotify:track:6SRsiMl7w1USE4mFqrOhHC')}>Play</button>
+        <img className="mediaplayer-header--img" src={spotify.nowPlaying.image}></img>
+        <div className='mediaplayer-header--about'>
+          <h1>{spotify.nowPlaying.title}</h1>
+          <h2>{spotify.nowPlaying.artist}</h2>
+        </div>
       </header>
+      {/* <p>Img and podcast info here</p>
+        <button onClick={() => spotify.spotifyPlayer.togglePlay()}>Toggle Playback</button>
+        <button onClick={() => spotify.play('spotify:track:6SRsiMl7w1USE4mFqrOhHC')}>Play</button> */}
     </div>
   );
 
